58fbb98eb075951a0e3aea0905133315311ddf51
Xiaojing Yan <xyan@brandeis.edu>
2020-04-03 14:54:15 -0400

    add button outline

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_002203) do
+ActiveRecord::Schema.define(version: 2020_04_01_000202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,13 +53,6 @@ ActiveRecord::Schema.define(version: 2020_04_01_002203) do
     t.integer "matched_user_id"
   end
 
-  create_table "driver_requests", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "request_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "makes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "request_id"
@@ -89,13 +82,6 @@ ActiveRecord::Schema.define(version: 2020_04_01_002203) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "matched_user_id"
-  end
-
-  create_table "rider_availabilities", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "availability_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
