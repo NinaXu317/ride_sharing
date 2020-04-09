3f26de6ed729a179888a59f650735e7cd78057d4
NinaXu317 <c1xu@brandeis.edu>
2020-04-09 11:26:21 -0400

    google map for searching

--- app/javascript/controllers/maps_controller.js
+++ app/javascript/controllers/maps_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["field", "map", "latitude", "longitude"]
+  static targets = ["field", "field", "map", "latitude", "longitude"]
 
   connect() {
     if (typeof (google) != "undefined"){
