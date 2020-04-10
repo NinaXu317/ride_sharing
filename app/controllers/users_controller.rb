98edf69141f9a49599efeb474b00857b815b0ac9
NinaXu317 <c1xu@brandeis.edu>
2020-04-10 14:27:42 -0400

    fix redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
     puts @user.is_driver
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
         format.html { redirect_to root_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
