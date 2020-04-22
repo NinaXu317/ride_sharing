9642b2b2494a53ff3f976ef62916c01afd03495f
Xiaojing Yan <xyan@brandeis.edu>
2020-04-21 22:58:56 -0400

    send text through request ride

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -12,11 +12,19 @@ class NotificationsController < ApplicationController
 
     def notify
       availability_id = params["availability_id"].to_i
+      availability = Availability.find(availability_id)
+      post = Post.find_by(availability_id: availability_id)
+      puts post.class
+      puts post.user_id
+      puts post.user_id.class
+
+      user = User.find(post.user_id)
       if params["is_send_notification"] == "true"
         puts "True"
       else
-        post = Post.find_by(availability_id: availability_id)
-        # puts post.id
+        twilio_client = TwilioClient.new
+        message = "\nRide Sharing: An availability for #{user.username} has been matched.\nThe trip starts at #{availability.start_street_address}, ends at #{availability.end_street_address}.\nThe trip time is #{availability.trip_time}. Text Y to accpet or N to ignore."
+        twilio_client.send_text(user, message)
       end
     end
 
