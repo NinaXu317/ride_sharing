e9a83a27b30a057c3ac1a3616098dbf77e457daa
Kun Li <kunli@Kuns-MBP.fios-router.home>
2020-05-10 14:38:16 -0400

    update test

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -5,21 +5,27 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:one)
+    @user = users(:two)
   end
   #
   test "should login in and go to search page" do
     get '/'
-    assert_response :success
+    assert_template 'static_pages/home'
+    # assert_response :success
 
     get new_user_registration_path
-    assert_response 200, status
+    # assert_response 200, status
     @user.confirm
     sign_in @user
     post "/users/sign_in"
     assert_redirected_to root_path
     get search_user_requests_url(@user.id)
-
     # assert_equal 200, status
+  end
+
+  test "rider should login and search" do
+    get '/'
+    assert_template 'static_pages/home'
   end
 
 end
