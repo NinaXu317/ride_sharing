d22d7460849f0335e260f42434f5f2419dcc857f
Xiaojing Yan <xyan@brandeis.edu>
2020-05-11 11:50:59 -0400

    update notification controller"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,24 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { :host => 'ride-sharing-demo.herokuapp.com', :protocol => 'http' }
+
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'yanxiaojingboston@gmail.com',
+      password:             '1234qwer@BR',
+      authentication:       'plain'
+  }
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
