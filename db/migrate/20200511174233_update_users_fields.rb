4c7ac9cecde92ca714a86e06cc4bab4af524c8c4
Kun Li <kunli@Kuns-MBP.fios-router.home>
2020-05-11 17:34:43 -0400

    update migrate

--- db/migrate/20200511174233_update_users_fields.rb
+++ db/migrate/20200511174233_update_users_fields.rb
@@ -1,7 +1,7 @@
 class UpdateUsersFields < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :confirmation_token, :string
-    add_column :users, :confirmed_at, :string
-    add_column :users, :confirmation_sent_at, :string
-  end
+  # def change
+  #   # add_column :users, :confirmation_token, :string
+  #   add_column :users, :confirmed_at, :string
+  #   add_column :users, :confirmation_sent_at, :string
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -153,24 +153,13 @@ ActiveRecord::Schema.define(version: 2020_05_11_174233) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-<<<<<<< HEAD
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-=======
-    t.datetime "remember_created_at"
->>>>>>> b650427abbb460b2264f1004750b84c354bb7d6e
     t.boolean "is_send_notification"
+    t.boolean "deactivated"
     t.string "encrypted_password", default: "", null: false
     t.bigint "phone_number"
-<<<<<<< HEAD
-=======
-    t.string "confirmation_token"
-    t.string "confirmed_at"
-    t.string "confirmation_sent_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> b650427abbb460b2264f1004750b84c354bb7d6e
   end
 
   create_table "vehicles", force: :cascade do |t|
