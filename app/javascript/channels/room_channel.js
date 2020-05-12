78195e28dec302d2ba6652fdf5c8e5f7baec4021
Kun Li <kunli@Kuns-MBP.fios-router.home>
2020-05-12 18:13:32 -0400

    update chat

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -11,18 +11,24 @@ consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
-    console.log(data.sender_id)
-    console.log($('form label').attr("data-current-user-id"))
+    console.log("data.sender_id: " + data.sender_id)
+    console.log("data-current-user-id: "+$('form label').attr("data-current-user-id"))
     // var sender = data.sender_id
     // var receiver = $( "#message_receiver_id" ).val()
-    if (data.sender_id == $('form label').attr("data-current-user-id")){
-      $('#message_holder').append('<div class="message" style="color: #ffffff;text-align:right;font-size:20px;border: 2px solid #dedede; background-color: blue;border-radius: 5px;padding: 10px;margin: 5% 5% 1% 5%;">' 
-      +data.content+'</div>'+'<i class="fas fa-paper-plane large" style="font-size: 3em"></i>' )
+    const myId = $('form label').attr("data-current-user-id");
+    console.log('myid'+myId)
+    // if (myId) {      
+    
+    if (data.sender_id == myId || data.receiver_id == myId){
+      if (data.sender_id == myId){
+        $('#message_holder').append('<div class="message" style="color: #ffffff;text-align:right;font-size:20px;border: 2px solid #dedede; background-color: blue;border-radius: 5px;padding: 10px;margin: 5% 5% 1% 5%;">' 
+        +data.content+'</div>'+'<i class="fas fa-paper-plane large" style="font-size: 3em"></i>' )
+      }
+      else {
+        $('#message_holder').append('<div class="message" style="text-align:left; font-size:20px;border: 2px solid #dedede; background-color: f1f1f1;border-radius: 5px;padding: 10px;margin: 5% 5% 1% 5%;">'+data.content+'</div>')
+      }
     }
-    else{
-      $('#message_holder').append('<div class="message" style="text-align:left; font-size:20px;border: 2px solid #dedede; background-color: f1f1f1;border-radius: 5px;padding: 10px;margin: 5% 5% 1% 5%;">'+data.content+'</div>')
-    }
-
+    
     console.log("!!!"+ data.sender_id)
     // Called when there's incoming data on the websocket for this channel
   }
